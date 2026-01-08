febb3a822108cdec916e3f4f1e1a7e8dc1135f9a
Dan Hulme <st@istic.org>
2026-01-08 22:38:09 +0000

    Make it work. There's a build task and it automatically opens the PDF.

--- README.md
+++ README.md
@@ -6,19 +6,8 @@ This is an extension to help you when editing LilyPond files in VS Code. It prov
 
 For most functionality you will need LilyPond installed. You can get it from https://lilypond.org/
 
-## Getting started
+## How to use
 
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
+- In the settings, set `lilypondStudio.executablePath` to the path to your LilyPond executable.
+- Open a .ly file.
+- Hit ctrl-shift-b to "run build task" and then choose LilyPond from the list. You can also set it as the default build task.
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,96 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	console.log('LilyPond Studio extension is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "lilypond-studio" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('lilypond-studio.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from LilyPond Studio!');
+	const taskProvider = vscode.tasks.registerTaskProvider('lilypond', {
+		provideTasks: () => {
+			return [
+				createLilypondTask('preview'),
+				createLilypondTask('publish')
+			];
+		},
+		resolveTask: () => {
+			return undefined;
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(taskProvider);
+
+	const taskEndListener = vscode.tasks.onDidEndTask(async (e) => {
+		if (e.execution.task.definition.type === 'lilypond') {
+			const editor = vscode.window.activeTextEditor;
+			if (editor && editor.document.languageId === 'lilypond') {
+				const filePath = editor.document.uri.fsPath;
+				const fileDir = path.dirname(filePath);
+				const fileName = path.basename(filePath, '.ly');
+				const pdfPath = path.join(fileDir, `${fileName}.pdf`);
+
+				await openPdfPreview(pdfPath);
+			}
+		}
+	});
+
+	context.subscriptions.push(taskEndListener);
+
+	const textEditorChangeListener = vscode.window.onDidChangeActiveTextEditor(editor => {
+		if (editor && editor.document.languageId === 'lilypond') {
+			vscode.commands.executeCommand('setContext', 'lilypondFileOpen', true);
+		} else {
+			vscode.commands.executeCommand('setContext', 'lilypondFileOpen', false);
+		}
+	});
+
+	context.subscriptions.push(textEditorChangeListener);
+
+	if (vscode.window.activeTextEditor?.document.languageId === 'lilypond') {
+		vscode.commands.executeCommand('setContext', 'lilypondFileOpen', true);
+	}
 }
 
-// This method is called when your extension is deactivated
+function createLilypondTask(taskType: 'preview' | 'publish'): vscode.Task {
+	const config = vscode.workspace.getConfiguration('lilypondStudio');
+	const lilypondPath = config.get<string>('executablePath') || 'lilypond';
+
+	const editor = vscode.window.activeTextEditor;
+	const filePath = editor?.document.uri.fsPath || '${file}';
+
+	const args = taskType === 'publish'
+		? ['-dno-point-and-click', filePath]
+		: [filePath];
+
+	const execution = new vscode.ShellExecution(lilypondPath, args);
+
+	const taskName = taskType === 'preview'
+		? 'Engrave (preview)'
+		: 'Engrave (publish)';
+
+	const task = new vscode.Task(
+		{ type: 'lilypond', taskType },
+		vscode.TaskScope.Workspace,
+		taskName,
+		'lilypond',
+		execution,
+		'$lilypond'
+	);
+
+	task.group = vscode.TaskGroup.Build;
+	task.presentationOptions = {
+		reveal: vscode.TaskRevealKind.Always,
+		panel: vscode.TaskPanelKind.Dedicated,
+		clear: true
+	};
+
+	return task;
+}
+
+async function openPdfPreview(pdfPath: string) {
+	const uri = vscode.Uri.file(pdfPath);
+	await vscode.commands.executeCommand('vscode.open', uri, {
+		viewColumn: vscode.ViewColumn.Beside,
+		preserveFocus: false
+	});
+}
+
 export function deactivate() {}
